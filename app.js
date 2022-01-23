cd53db66b605b8725f0640bed1fbfa627383d78f
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-23 19:06:12 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 // solution_1
-function maxTriSum(numbers){
-  const sorted= [...new Set(numbers.sort((a,b)=>b-a))]
- return sorted[0]+sorted[1]+sorted[2]
-}
+const myLanguages=results=> Object.keys(results)
+.filter(val=>results[val]>59)
+.sort((a,b)=>results[b]-results[a])
+
 // solution_2
-function maxTriSum(numbers){
-  return ([...new Set(numbers)]).sort((a,b) => a-b).slice(-3).reduce((a,b) => a+b);
+function myLanguages(results) {
+  return Object.keys(results).filter(r => results[r] > 59).sort((a,b) => results[b] - results[a]);
 }
 
 // solution_3
 
-function maxTriSum(numbers){
-  numbers.sort((a, b) => b - a)
-  let arr = numbers.filter((e, i) => numbers.indexOf(e) === i)
-  return arr[0] + arr[1] + arr[2]
+function myLanguages(results) {
+  return Object.entries(results).sort((a,b) => b[1] - a[1]).filter( item => item[1] >= 60).map(item => item[0]);
 }
 
-console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]) )
-console.log(maxTriSum([-14,-12,-7,-42,-809,-14,-12]) )
-console.log(maxTriSum([-13,-50,57,13,67,-13,57,108,67]) )
+console.log(myLanguages({"Java" : 10, "Ruby" : 80, "Python" : 65}), ["Ruby", "Python"])
+
 
