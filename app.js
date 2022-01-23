c472d60b87d0018bef254851917d90d98e918f3a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-23 17:12:50 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 // solution_1
-const findMissing = (arr1, arr2) => {
-  arr1 = arr1.sort();
-  arr2 = arr2.sort();
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] != arr2[i]) return arr1[i];
-  }
-}
+const getDecimal=n=> +("0."+String(n).split('.')[1])||0
 // solution_2
-
-const sum = arr => arr.reduce((a, b) => a + b, 0);
-
-const findMissing = (arr1, arr2) => sum(arr1) - sum(arr2);
-
-  // solution_3
-  const findMissing = (arr1, arr2) =>
-  arr1.reduce((pre, val) => pre + val) - arr2.reduce((pre, val) => pre + val, 0);
-
-  // solution_4
-
-  function findMissing(arr1, arr2) {
-    return arr1.sort().filter((e,i) => arr2.sort()[i] !== e)[0]
-    }
-  // solution_5
-  function findMissing(arr1, arr2) {
-    return arr1.reduce( (s, el) => s + el, 0) - arr2.reduce( (s, el) => s + el, 0);
-  }
-  // solution_6
-
-  const findMissing=(arr1, arr2)=> arr1.sort()
-.filter((e,i) => arr2.sort()[i] !== e)[0]
+function getDecimal(n){
+  return Math.abs(n%1);
+}
+// solution_3
+const getDecimal = n => +`${String(n).slice(String(n).indexOf('.'))}`;
 
 
-console.log(findMissing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) )
+console.log(getDecimal(-0.34) )
+console.log(getDecimal(2.4) )
+console.log(getDecimal(223) )
+
