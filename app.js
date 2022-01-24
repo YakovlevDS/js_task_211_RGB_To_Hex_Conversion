6ecaccd5fec3ad44f7bde2babe3b9c4b7d7db664
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-24 12:21:18 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 // solution_1
-const nicknameGenerator=name=>{
-  const vowel= ['e','i','u','o','a','j']
-  if (name.length<4 ){
-    return "Error: Name too short"
-  }
-  
-  if (vowel.includes(name[2])) {
-      return name.slice(0,4)
+const toHex = num => {
+  if (num< 1)   return '00';
+  if (num> 255)  return 'FF';
+ let numHex = num.toString(16).toUpperCase();
+ return numHex.length == 1 ? 0 + numHex : numHex;
 }
-  return name.slice(0,3)
-}
+const rgb= (r, g, b) =>toHex(r) + toHex(g) + toHex(b);
 // solution_2
-
-const nicknameGenerator = (name) => name.length > 3 
-  ? name.slice(0,  3 + 'aeiou'.includes(name[2])) 
-  : 'Error: Name too short'
-// solution_3
-
-function nicknameGenerator(name) {
-  if(name.length <= 3) {return 'Error: Name too short';}
-  return !/[aeiou]/i.test(name[2]) ? name.slice(0,3) : name.slice(0,4);
+function rgb(r, g, b){
+  return [r,g,b].map(x => { 
+    if(x > 255) return 'FF'
+    else if(x < 1) return '00'
+    else return x.toString(16).toUpperCase()}).join('') 
 }
-// solution_4
-const nicknameGenerator = n => n.length > 3 ? n.replace(/(..[^aeiou]|^.{4}).*/gi, '$1') : 'Error: Name too short';
 
-console.log(nicknameGenerator("Sayaev"))
+
+
+console.log(toHex(17, 45, 67))
